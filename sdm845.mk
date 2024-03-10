5ddaef3c14b3dfc8712e42e3e106f04c6f6cf977
Naruto-242 <Naruto242@proton.me>
2024-03-10 07:09:25 +0000

    Revert "sdm845-common: Move to QTI health AIDL service"
    This reverts commit cf820100b74ffdcc6f9563f2dc550b36c87e11e8.

--- sdm845.mk
+++ sdm845.mk
@@ -181,8 +181,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery
+    android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
